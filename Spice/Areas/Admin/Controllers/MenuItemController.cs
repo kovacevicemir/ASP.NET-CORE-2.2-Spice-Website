02e08400385b13de1ddb84d660a43ac591e98b77
emirkovacevic <emirkovacevic92@gmail.com>
2019-08-05 22:48:44 +1000

    MenuItem Added

---
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public MenuItemController(ApplicationDbContext db, IHostingEnvironment hostinEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostinEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+    }
+}
